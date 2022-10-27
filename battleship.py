2a15f065a32d67d19fe91b7233a4186eb867e727
LoaffPred <icosidiouxxix@gmail.com>
2022-10-27 23:53:33 +0800

    alpha check issues[todo]

--- battleship.py
+++ battleship.py
@@ -1,4 +1,4 @@
-main_board = [
+bview_board = [
     ["| X |", " A |", " B |", " C |", " D |", " E |", " F |", " G |", " H |", " I |", " J |"],
     ["| 1 |", "A1 |", "B1 |", "C1 |", "D1 |", "E1 |", "F1 |", "G1 |", "H1 |", "I1 |", "J1 |"],
     ["| 2 |", "A2 |", "B2 |", "C2 |", "D2 |", "E2 |", "F2 |", "G2 |", "H2 |", "I2 |", "J2 |"],
@@ -9,7 +9,21 @@ main_board = [
     ["| 7 |", "A7 |", "B7 |", "C7 |", "D7 |", "E7 |", "F7 |", "G7 |", "H7 |", "I7 |", "J7 |"],
     ["| 8 |", "A8 |", "B8 |", "C8 |", "D8 |", "E8 |", "F8 |", "G8 |", "H8 |", "I8 |", "J8 |"],
     ["| 9 |", "A9 |", "B9 |", "C9 |", "D9 |", "E9 |", "F9 |", "G9 |", "H9 |", "I9 |", "J9 |"],
-    ["|10 |", "A10|", "B10|", "C10|", "D10|", "E10|", "F10|", "G10|", "H10|", "I10|", "J10|",],
+    ["|10 |", "A10|", "B10|", "C10|", "D10|", "E10|", "F10|", "G10|", "H10|", "I10|", "J10|"],
+]
+
+rview_board = [
+    ["| X |", " A |", " B |", " C |", " D |", " E |", " F |", " G |", " H |", " I |", " J |"],
+    ["| 1 |", "A1 |", "B1 |", "C1 |", "D1 |", "E1 |", "F1 |", "G1 |", "H1 |", "I1 |", "J1 |"],
+    ["| 2 |", "A2 |", "B2 |", "C2 |", "D2 |", "E2 |", "F2 |", "G2 |", "H2 |", "I2 |", "J2 |"],
+    ["| 3 |", "A3 |", "B3 |", "C3 |", "D3 |", "E3 |", "F3 |", "G3 |", "H3 |", "I3 |", "J3 |"],
+    ["| 4 |", "A4 |", "B4 |", "C4 |", "D4 |", "E4 |", "F4 |", "G4 |", "H4 |", "I4 |", "J4 |"],
+    ["| 5 |", "A5 |", "B5 |", "C5 |", "D5 |", "E5 |", "F5 |", "G5 |", "H5 |", "I5 |", "J5 |"],
+    ["| 6 |", "A6 |", "B6 |", "C6 |", "D6 |", "E6 |", "F6 |", "G6 |", "H6 |", "I6 |", "J6 |"],
+    ["| 7 |", "A7 |", "B7 |", "C7 |", "D7 |", "E7 |", "F7 |", "G7 |", "H7 |", "I7 |", "J7 |"],
+    ["| 8 |", "A8 |", "B8 |", "C8 |", "D8 |", "E8 |", "F8 |", "G8 |", "H8 |", "I8 |", "J8 |"],
+    ["| 9 |", "A9 |", "B9 |", "C9 |", "D9 |", "E9 |", "F9 |", "G9 |", "H9 |", "I9 |", "J9 |"],
+    ["|10 |", "A10|", "B10|", "C10|", "D10|", "E10|", "F10|", "G10|", "H10|", "I10|", "J10|"],
 ]
 
 blue_board = [
@@ -23,7 +37,7 @@ blue_board = [
     ["| 7 |", "A7 |", "B7 |", "C7 |", "D7 |", "E7 |", "F7 |", "G7 |", "H7 |", "I7 |", "J7 |"],
     ["| 8 |", "A8 |", "B8 |", "C8 |", "D8 |", "E8 |", "F8 |", "G8 |", "H8 |", "I8 |", "J8 |"],
     ["| 9 |", "A9 |", "B9 |", "C9 |", "D9 |", "E9 |", "F9 |", "G9 |", "H9 |", "I9 |", "J9 |"],
-    ["|10 |", "A10|", "B10|", "C10|", "D10|", "E10|", "F10|", "G10|", "H10|", "I10|", "J10|",],
+    ["|10 |", "A10|", "B10|", "C10|", "D10|", "E10|", "F10|", "G10|", "H10|", "I10|", "J10|"],
 ]
 red_board = [
     ["| X |", " A |", " B |", " C |", " D |", " E |", " F |", " G |", " H |", " I |", " J |"],
@@ -36,145 +50,167 @@ red_board = [
     ["| 7 |", "A7 |", "B7 |", "C7 |", "D7 |", "E7 |", "F7 |", "G7 |", "H7 |", "I7 |", "J7 |"],
     ["| 8 |", "A8 |", "B8 |", "C8 |", "D8 |", "E8 |", "F8 |", "G8 |", "H8 |", "I8 |", "J8 |"],
     ["| 9 |", "A9 |", "B9 |", "C9 |", "D9 |", "E9 |", "F9 |", "G9 |", "H9 |", "I9 |", "J9 |"],
-    ["|10 |", "A10|", "B10|", "C10|", "D10|", "E10|", "F10|", "G10|", "H10|", "I10|", "J10|",],
+    ["|10 |", "A10|", "B10|", "C10|", "D10|", "E10|", "F10|", "G10|", "H10|", "I10|", "J10|"],
 ]
-# might change guides to tuples to prevent modifying
 
 
-# try to clean
-# def deploy_ship(length, board):
-#     while True:
-#         try:
-#             for row in board:
-#                 for cell in row:
-#                     print(cell, end="")
-#                 print()
-#             coords = (
-#                 input(f"Enter coordinates for ship of length {length}: ")
-#                 .upper()
-#                 .split(",")
-#             )
-#             if check_length(length, coords):
-                
-
-
-#                 break
-#             else:
-#                 print(f"Enter {length} coordinates only.")
-#         except (ValueError, IndexError):
-#             print("Invalid input")
-
-    # return coords_index
-
-
-def init_ship(self, length, board):
-    while True:
-        try:
-            Player.print_board(board)
-            coords = (input(f"Enter coordinates for ship of length {self.length}: ").upper().split(","))
-            if Ship.check_length(length, coords):
-                Ship.deploy_ship(coords, board)
-                break
-            else:
-                print(f"Enter {length} coordinates only.")
-        except (ValueError, IndexError):
-            print("Invalid input")
-    self.coords = coords
-
-def deploy_ship(coords, board):
-    coords_index = []
-    for pair in coords:
-        y, x = Ship.coords_to_index(pair)
-        coords_index.append((y, x))
-        board[y][x] = f" S |"
-
-
-
-def check_length(length, coords):
-    if len(coords) == length:
-        return True
-    else:
-        return False
-
-
-def coords_to_index(coords):
-    letters = ["X", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-    x = coords[:1]
-    y = coords[1:]
-    for set in enumerate(letters):
-        if x in set:
-            x = set[0]
-        else:
-            pass
-    return int(y), x
-        
-
-
-class Player:
-    def __init__(self, board, color):
-        self.board = board
-        self.color = color
-        self.ships = {}
-            # self.ship_2 : Ship(self.color, 2, self.board),
-            # self.ship_3a : Ship(self.color, 3, self.board),
-            # self.ship_3b : Ship(self.color, 3, self.board),
-            # self.ship_4 : Ship(self.color, 4, self.board),
-            # self.ship_5 : Ship(self.color, 5, self.board)
-        
-
-    @staticmethod
-    def print_board(board):
+def print_board(board):
         for row in board:
             for cell in row:
                 print(cell, end="")
             print()
         print()
+        
+class Player:
+    def __init__(self, color, board):
+        self.board = board
+        self.color = color
+        self.ships = {}
+        
+    def get_lives(self):
+        return len(self.ships)
+    
+    def get_board(self):
+        return self.board
+            
+    def add_ship(self, name, atts):
+        self.ships[name] = atts
+        
+# TODO #6
+class Ship:
+    def __init__(self, name, length, player):
+        self.name = name
+        self.length = length
+        self.atts = {
+            "lives" : self.length,
+            "coords" : Ship.init_ship(self.length, player.get_board())
+        }
+        player.add_ship(name, self.atts)
+        
+    def init_ship(length, board):
+        while True:
+            try:
+                coords = (input(f"Enter coordinates for ship of length {length}: ").upper().split(","))
+                if Ship.check_length(length, coords):
+                    Ship.deploy_ship(coords, board)
+                    print_board(board)
+                    break
+                else:
+                    print(f"Enter {length} coordinates only.")
+            except (ValueError, IndexError):
+                print("Invalid input")
+        return coords
+    
+    def deploy_ship(coords, board):
+        coords_index = []
+        for pair in coords:
+            y, x = Ship.coords_to_index(pair)
+            coords_index.append((y, x))
+            board[y][x] = f" S |"
+    
+    @staticmethod
+    def check_length(length, coords):
+        if len(coords) == length:
+            return True
+        else:
+            return False
+    
+    @staticmethod
+    def coords_to_index(coords):
+        letters = ["X", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        x = coords[:1]
+        y = coords[1:]
+        for set in enumerate(letters):
+            if x in set:
+                x = set[0]
+                break
+        return int(y), x
+    
 
-
-# two main boards
-# def player_shoot(target):
-#     Player.print_board(main_board)
-#     cell = input("Enter target cell coordinates: ")
-#     y, x = Ship.coords_to_index(cell)
-#     if "S" in target.board[y][x]:
-#         print("HIT!")
-#     for ships_, ship in target.ships.items():
-#         if ship.coords == cell:
-#             ship.lives -= 1
-
+def player_shoot(target, shooter_board):
+    cell = input("Enter target cell coordinates: ").upper()
+    y, x = Ship.coords_to_index(cell)
+    action = "miss"
+    if "S" in target.board[y][x]:
+        print("HIT!")
+        action = "hit"
+        hit_or_miss(shooter_board, y, x, action)
+        for ship, atts in target.ships.items():
+            if cell in atts["coords"]: # might remove
+                # atts["coords"].remove(cell)
+                atts["lives"] -= 1
+    else:
+        hit_or_miss(shooter_board, y, x, action)
+        print("MISS!")
+    print_board(shooter_board)
+                
+                
+def hit_or_miss(board, y, x, action):
+    if action == "hit":
+        board[y][x] = " H |"
+    if action == "miss":
+        board[y][x] = " X |"
+        
+def check_lives(target, no_ships):
+    lives = no_ships
+    # print("Lives:", lives)
+    for ship, atts in target.ships.items():
+        # print(target.ships.items())
+        # print(ship, atts)
+        if atts["lives"] == 0:
+            lives -= 1
+            print("You sunk my battleship!")
+            print(f"{target.color} player has {lives} ships remaining.")
+    if lives == 0:
+        print("Player", target.color, "has been defeated.")
+        return True
+    
+            
+# while loop ; func checking both players' lives
 
 
 
 def main():
     print("~~~~~~~~~~~~~~~Blue player setup~~~~~~~~~~~~~~~")
-    player_blue = Player(blue_board, "Blue")
-    b_ship_2 = {
-        "lives" : 0,
-        "coords" : []
-    }
-
-
+    player_blue = Player("Blue", blue_board)
+    print_board(blue_board)
+    b_ship_2 = Ship("ship_2", 2, player_blue)
+    b_ship_3a = Ship("ship_3a", 3, player_blue)
+    b_ship_3b = Ship("ship_3b", 3, player_blue)
+    b_ship_4 = Ship("ship_4", 4, player_blue)
+    b_ship_5 = Ship("ship_5", 5, player_blue)
+    input("Press enter to confirm.")
     print("\n" * 100)
-
-
-
-
-
+    
+    
     print("~~~~~~~~~~~~~~~Red player setup~~~~~~~~~~~~~~~")
-    player_red = Player(red_board, "Red")
+    player_red = Player("Red", red_board)
+    print_board(red_board)
+    r_ship_2 = Ship("ship_2", 2, player_red)
+    r_ship_3a = Ship("ship_3a", 3, player_red)
+    r_ship_3b = Ship("ship_3b", 3, player_red)
+    r_ship_4 = Ship("ship_4", 4, player_red)
+    r_ship_5 = Ship("ship_5", 5, player_red)
+    input("Press enter to confirm.")
     print("\n" * 100)
-
-
-
-
-
-
+    
+    
     print("~~~~~~~~~~~~~~~Battle phase~~~~~~~~~~~~~~~")
-    # player_shoot(player_red)
-    # print(player_red.ships)
+    
+    print("Red Player start")
+    while True:
+        print("~~~~~~~~~~~~~~~Red player's turn~~~~~~~~~~~~~~~")
+        print_board(rview_board)
+        player_shoot(player_blue, rview_board)
+        if check_lives(player_blue, player_blue.get_lives()):
+            break
 
+        print("~~~~~~~~~~~~~~~Blue player's turn~~~~~~~~~~~~~~~")
+        print_board(bview_board)
+        player_shoot(player_red, bview_board)
+        if check_lives(player_red, player_red.get_lives()):
+            break
 
-
-
+    
 
 main()
