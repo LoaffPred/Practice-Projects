f514134deb70e71d7b40a7e2e9c49527d5b80a9d
LoaffPred <icosidiouxxix@gmail.com>
2022-12-09 23:27:42 +0800

    made functions

--- Battleship/battleshipv2.0.py
+++ Battleship/battleshipv2.0.py
@@ -1,11 +1,19 @@
 import json
 
-# file directory might change
-with open("Battleship\\red_board.json", "r") as f:
-    board = json.load(f)
+
+def get_board(board_name):
+    # file directory might change
+    with open(f"Battleship\\{board_name}.json", "r") as f:
+        return json.load(f)
 
 
-for row in board:
-    for col in row:
-        print(col, end="")
-    print()
+def write_board(data, board_name):
+    with open(f"Battleship\\{board_name}.json", "w") as f:
+        json.dump(data, f)
+
+
+def print_board(board):
+    for row in board:
+        for col in row:
+            print(col, end="")
+        print()
